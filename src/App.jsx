92ad4f8de11334f0f22ef6d1ed0175c87542e40e
Ethan <ejbolts@hotmail.com>
2024-07-01 08:42:39 +1000

    Basic dependencies setup, Add react Leaflet and  IP Geolocation API

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import MapComponent from "./components/MapComponent";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  async function getIP() {
+    const response = await fetch(
+      "https://geo.ipify.org/api/v2/country?apiKey=at_wNXjFcNTx9ROaiEQlRqnx9uqUR5kI&ipAddress=8.8.8.8"
+    );
+    const data = response.json();
+    console.log(data);
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <MapComponent />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={() => getIP()}>Find IP</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/MapComponent.jsx
@@ -0,0 +1,38 @@
+import { MapContainer, TileLayer, Marker, Popup, Tooltip } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
+const MapComponent = () => {
+  const position = [-27.4705, 153.026];
+
+  return (
+    <MapContainer
+      center={position}
+      zoom={13}
+      style={{ height: "100vh", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+        <Tooltip>{position}r</Tooltip>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default MapComponent;
